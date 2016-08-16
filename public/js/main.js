c5150aff7d75f525a93acd68fd6b11596a1f3710
shirasaka <shirasaka@n-create.co.jp>
2016-08-16 20:54:49 +0900

    移動した間取がくっつくように修正

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 var madori = {
+    version: '0.0.1',
     _default: 100,
+    _scale: 1,
+    _locate: {x: [], y: []},
     _units: {1.70: '団地間', 1.76: '江戸間', 1.82: '中京間', 1.91: '京間'},
     _types: {
         1: {name: '洋室', color: '#bcaaa4'},
@@ -12,8 +15,7 @@ var madori = {
         8: {name: '玄関', color: '#ce93d8'},
         9: {name: 'その他', color: '#f44336'},
     },
-    _scale: 1,
-    _drag: {x: 0, y: 0, isMove: false},
+    _drag: {x: 0, y: 0, isMove: false, lock: false},
     _stage: null,
     init: function() {
         this._stage = new createjs.Stage('madori');
@@ -24,6 +26,7 @@ var madori = {
         $('#add').on('click', this.add.bind(this));
         $('#import').on('change', this.importFile.bind(this));
         $('#export').on('click', this.exportFile.bind(this));
+        $('#menu').sideNav();
         $('select').material_select();
 
         if (this._default > $(document).width() / 15) this._scale = $(document).width() / 15 / this._default;
@@ -32,6 +35,10 @@ var madori = {
     update: function() {
         this._stage.update();
         this.setTubo();
+    },
+    remove: function(c) {
+        this._clearLocate(c);
+        this._stage.removeChild(c);
     },
     setTubo: function() {
         var tubo = 0;
@@ -56,7 +63,19 @@ var madori = {
         return size / length / 2;
     },
     add: function() {
-        this.change(this.create(100, 100, this.getLength(1, 1), 1, '1.82', 1));
+        this.change({x: 100, y: 100, size: 1, height: 1, width: 0.5, unit: '1.82', type: 1, _init: true});
+    },
+    _setLocate(c) {
+        this._locate.x.push(c.x);
+        this._locate.y.push(c.y);
+        this._locate.x.push(c.x + c.width * c.unit * this._default * this._scale);
+        this._locate.y.push(c.y + c.height * c.unit * this._default * this._scale);
+    },
+    _clearLocate(c) {
+        this._locate.x.splice(this._locate.x.indexOf(c.x), 1);
+        this._locate.y.splice(this._locate.y.indexOf(c.y), 1);
+        this._locate.x.splice(this._locate.x.indexOf(c.x + c.width * c.unit * this._default * this._scale), 1);
+        this._locate.y.splice(this._locate.y.indexOf(c.y + c.height * c.unit * this._default * this._scale), 1);
     },
     create: function(x, y, width, height, unit, type) {
         var container = new createjs.Container();
@@ -74,12 +93,13 @@ var madori = {
         container.size = width * height * 2;
         container.unit = unit;
         container.type = type;
-        obj.graphics.beginFill(this._types[type].color).drawRect(0, 0, container.width * unit * this._default, container.height * unit * this._default);
+        obj.graphics.beginFill(this._types[type].color).drawRect(0, 0, width * unit * this._default, height * unit * this._default);
         obj.graphics.endFill();
-        lineTop.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(0, 0).lineTo(container.width * unit * this._default, 0).endStroke();
-        lineLeft.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(0, 0).lineTo(0, container.height * unit * this._default).endStroke();
-        lineRight.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(container.width * unit * this._default, 0).lineTo(container.width * unit * this._default, container.height * unit * this._default).endStroke();
-        lineBottom.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(0, container.height * unit * this._default).lineTo(container.width * unit * this._default, container.height * unit * this._default).endStroke();
+        lineTop.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(0, 0).lineTo(width * unit * this._default, 0).endStroke();
+        lineLeft.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(0, 0).lineTo(0, height * unit * this._default).endStroke();
+        lineRight.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(width * unit * this._default, 0).lineTo(width * unit * this._default, height * unit * this._default).endStroke();
+        lineBottom.graphics.beginStroke('Black').setStrokeStyle(3).moveTo(0, container.height * unit * this._default).lineTo(width * unit * this._default, height * unit * this._default).endStroke();
+        this._setLocate(container);
 
         $(container).on('mousedown', {c: container}, this.moveStart.bind(this));
         $(container).on('pressmove', {c: container}, this.move.bind(this));
@@ -106,8 +126,9 @@ var madori = {
         return container;
     },
     _resize: function(c, line, diff) {
-        var offset = c.unit * 0.025 * this._default;
+        var offset = c.unit * 0.25 * this._default;
 
+        this._drag.lock = true;
         if (Math.abs(diff) > offset) {
             if (c.width < c.height && (diff > 0 || c.width > 0.25)) {
                 c.width += 0.25 * (diff > 0 ? 1 : -1);
@@ -120,8 +141,9 @@ var madori = {
             }
             if (c.width && c.height) {
                 $(line).off('pressmove');
-                this._stage.removeChild(c);
+                this.remove(c);
                 this.create(c.x, c.y, c.width, c.height, c.unit, c.type);
+                this._drag.lock = false;
             }
         }
     },
@@ -148,8 +170,24 @@ var madori = {
     move: function(e) {
         var c = e.data.c;
 
+        if (this._drag.lock) return;
+        this._clearLocate(c);
         c.x = this._stage.mouseX - this._drag.x;
         c.y = this._stage.mouseY - this._drag.y;
+
+        this._locate.x.find(function(elm) {
+            if (Math.abs(elm - c.x) < 10) c.x = elm;
+            else if (Math.abs(elm - c.x - c.width * c.unit * this._default) < 10) c.x = elm - c.width * c.unit * this._default + 3;
+            else return false;
+            return true;
+        }, this);
+        this._locate.y.find(function(elm) {
+            if (Math.abs(elm - c.y) < 10) c.y = elm;
+            else if (Math.abs(elm - c.y - c.height * c.unit * this._default) < 10) c.y = elm - c.height * c.unit * this._default + 3;
+            else return false;
+            return true;
+        }, this);
+        this._setLocate(c);
         this._drag.isMove = true;
         this.update();
     },
@@ -157,7 +195,7 @@ var madori = {
         var c = e.data.c;
 
         $('body').css('cursor', 'pointer');
-        if (!this._drag.isMove) this.change(c);
+        if (!this._drag.isMove && !this._drag.lock) this.change(c);
     },
     change: function(c) {
         $('#form').openModal();
@@ -167,12 +205,13 @@ var madori = {
         $('select').material_select('update');
         $('#change').off('click');
         $('#change').on('click', () => {
-            this._stage.removeChild(c);
+            if (!c._init) this.remove(c);
             this.create(c.x, c.y, this.getLength($('#size').val(), c.height), c.height, $('#unit').val(), $('#type').val());
             $('#form').closeModal();
+            $('#side').sideNav('hide');
         })
         $('#remove').on('click', () => {
-            this._stage.removeChild(c);
+            this.remove(c);
             this.update();
             $('#form').closeModal();
         })
@@ -184,19 +223,20 @@ var madori = {
         file.onload = () => {
             var json = JSON.parse(file.result);
 
-            for (var i = 0; i < this._stage.children.length; i++) {
-                this._stage.removeChild(this._stage.children[i]);
+            for (var i = this._stage.children.length - 1; i >= 0; i--) {
+                this.remove(this._stage.children[i]);
             }
             for (var i = 0; i < json.data.length; i++) {
-                this.create(json.data[i].x, json.data[i].y, json.data[i].width, json.data[i].height, json.data[i].unit, json.data[i].type);
+                this.create(json.data[i].x * this._scale, json.data[i].y * this._scale, json.data[i].width, json.data[i].height, json.data[i].unit, json.data[i].type);
             }
+            $('#side').sideNav('hide');
         }
     },
     exportFile: function() {
-        var json = {version: '0.0.1', data: []};
+        var json = {version: this.version, data: []};
         for (var i = 0; i < this._stage.children.length; i++) {
             var c = this._stage.children[i];
-            json.data.push({x: c.x, y: c.y, width: c.width, height: c.height, unit: c.unit, type: c.type});
+            json.data.push({x: c.x / this._scale, y: c.y / this._scale, width: c.width, height: c.height, unit: c.unit, type: c.type});
         }
         window.location.href = window.URL.createObjectURL(new Blob([JSON.stringify(json)], {type: 'application/octet-stream'}));
     }
